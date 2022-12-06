5f7078576315ff25e53ccf58d941897172dc48c0
Roland Greim <github@rolandgreim.de>
2022-12-06 21:34:31 +0100

    :sparkles: Solved Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,43 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var p = 0
+
+        input.forEach {
+            val num = it.toIntOrNull()
+            if (num == null) {
+                if (p > max) {
+                    max = p
+                }
+                p = 0
+            } else {
+                p += num
+            }
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val top = sortedSetOf<Int>()
+        var p = 0
+
+        input.forEach {
+            val num = it.toIntOrNull()
+            if (num == null) {
+
+                top.add(p)
+                p = 0
+            } else {
+                p += num
+            }
+        }
+        return top.toList().takeLast(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
